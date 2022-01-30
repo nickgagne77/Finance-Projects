ccaa08dbae4bff6f91e23151acbc4e270cb1cc84
nickgagne77 <nick.gagne77@gmail.com>
2022-01-30 16:09:14 -0500

    Snowman js

--- Into_to_JS.js
+++ Into_to_JS.js
@@ -1,2 +1,15 @@
-/*Intro_to_JS/*
+/*Intro_to_JS*/
+
+//Snowman basic:
+
+//bottom circle: 
+ellipse (200, 300, 150, 150);
+
+//middle circle: 
+ellipse(200, 200, 100, 100);
+
+// top circle: 
+ellipse(200, 140, 75, 75);
+
+
 
